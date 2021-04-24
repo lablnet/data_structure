67de8ad1d00c281b9449b21517a38045a5f43656
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-04-24 16:49:26 +0500

    add queue executeable

---
+++ queue_main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Queue/Queue.cpp"
+
+int main() {
+
+    // Queue
+    Queue<int> q;
+    q.enqueue(10, 10);
+    q.enqueue(4, 1);
+    q.enqueue(6, 2);
+    q.enqueue(3, 3);
+    std::cout << "Size: " << q.size() << std::endl;
+    std::cout << "Dequeue: " << q.dequeue() << std::endl;
+    return 0;
+}
