c6e36867f4e9a44890cbee81689e56b3bc50c09d
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-05-05 08:20:40 +0500

    init hash table

---
+++ HashTable/HashTable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hp on 5/5/2021.
+//
+
+#include "HashTable.h"
---
+++ example/hash_table_example.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../HashTable/HashTable.cpp"
+
+int main()
+{
+    std::cout << "Hello Hash Tables " << std::endl;
+    return 0;
+}
