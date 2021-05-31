69eccb0cb357d64fb6d4fd881a6c026c2106951e
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-05-31 17:25:35 +0500

    Create BstUtility.h

---
+++ trees/BST/BstUtility.h
@@ -0,0 +1,21 @@
+//
+// Created by Umer on 5/17/2021.
+//
+
+#ifndef DOUBLYLINKEDLIST_H_BSTUTILITY_H
+#define DOUBLYLINKEDLIST_H_BSTUTILITY_H
+
+/**
+ * Get the max value.
+ *
+ * @param a first number.
+ * @param b second number.
+ *
+ * @since 1.0.0
+ * @return int
+ */
+inline int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+#endif //DOUBLYLINKEDLIST_H_BSTUTILITY_H
