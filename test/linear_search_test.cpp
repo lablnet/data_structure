a3931ec7ac0aae4be4ff4689d53c81274eda9243
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-06-07 15:54:22 +0500

    refactor

--- test/linear_search_test.cpp
+++ test/linear_search_test.cpp
@@ -11,7 +11,7 @@ std::vector<int> list;
 for (int i = 0; i <= 10; i++) {
 list.push_back(i);
 }
-ASSERT_EQ(3, LinearSearch(list, 3));
+ASSERT_EQ(3, linear_search(list, 3));
 }
 
 int main(int argc, char **argv) {
